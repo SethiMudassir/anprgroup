2f01a55542411f7cc8f8f03366ce3c4164ca65b0
anprgroup <anprgroup@c5bc3a2d-84d0-c7ca-2176-b34216832995>
2010-11-27 06:04:58 +0000

    Dongnt00292 update

---
+++ trunk/ anprgroup/Backup/Management/Parking regularTicket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class ParkingRegularTicket : Form
+    {
+        public ParkingRegularTicket()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox24_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}

e7a4eea614d13511da6a86dd2f0bf6a743c09fba
anprgroup <anprgroup@c5bc3a2d-84d0-c7ca-2176-b34216832995>
2010-11-28 06:16:04 +0000

    Dongnt: update FrameMain.cs and Resources

--- trunk/ anprgroup/Management/Program.cs
+++ trunk/ anprgroup/Management/Program.cs
@@ -15,7 +15,7 @@ namespace WindowsFormsApplication1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainFrame());
+            Application.Run(new FrameMain());
         }
     }
 }

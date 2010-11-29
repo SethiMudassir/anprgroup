babf274a39838e112a2f358b865b2a3c8680b926
anprgroup <anprgroup@c5bc3a2d-84d0-c7ca-2176-b34216832995>
2010-11-29 07:36:31 +0000

    Ngoc commit Report_Daily

--- anprgroup2/Management/Report_dailly.Designer.cs
+++ anprgroup2/Management/Report_dailly.Designer.cs
@@ -29,19 +29,20 @@
         private void InitializeComponent()
         {
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
-            this.Report_daily1 = new WindowsFormsApplication1.Report_daily();
+            this.ReportDailly1 = new WindowsFormsApplication1.ReportDailly();
             this.SuspendLayout();
             // 
             // crystalReportViewer1
             // 
             this.crystalReportViewer1.ActiveViewIndex = 0;
+            this.crystalReportViewer1.AutoSize = true;
             this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
             this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.ReportSource = this.Report_daily1;
-            this.crystalReportViewer1.Size = new System.Drawing.Size(284, 262);
+            this.crystalReportViewer1.ReportSource = this.ReportDailly1;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(639, 314);
             this.crystalReportViewer1.TabIndex = 0;
             this.crystalReportViewer1.Load += new System.EventHandler(this.crystalReportViewer1_Load_1);
             // 
@@ -49,19 +50,22 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(639, 314);
             this.Controls.Add(this.crystalReportViewer1);
             this.Name = "Report_dailly";
             this.Text = "Report_dailly";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Report_dailly_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
-        private Report_daily Report_daily1;
+        
+        private ReportDailly ReportDailly1;
 
     }
 }
